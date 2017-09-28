3559e3e677ad57ebe6b292a7a3941ca4eb247d53
Arthur Bonavita <arthur.bonavita@gmail.com>
2017-09-28 18:27:59 +0200

    add all the files to the rep

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$( document ).ready(function() {
+    $(".fade").fadeTo(1000,1,'swing');
+
+$('.hideme').css({"position":"relative","opacity": 0,  "top":"+=50"});
+
+    $(window).scroll( function(){
+
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight() -300;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+
+                //
+                $(this).animate({
+                    top:0,
+
+                    opacity:1
+                },500);
+
+
+            }
+
+        });
+
+    });
+
+
+});
